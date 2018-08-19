912bdb1510bc8777222e3baa6382fd5a00c94193
becharvey93 <becharvey93@users.noreply.github.com>
2018-08-19 19:56:59 +1000

    added components for logging in, viewing groups and viewing channels

--- assignment-one/src/app/app.module.ts
+++ assignment-one/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ChannelsComponent } from './channels/channels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    GroupsComponent,
+    ChannelsComponent
   ],
   imports: [
     BrowserModule,
