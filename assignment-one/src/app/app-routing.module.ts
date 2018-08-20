83c5d31ec94e1f3c937835f67f2105c27abbd5e1
becharvey93 <becharvey93@users.noreply.github.com>
2018-08-20 11:31:43 +1000

    added routes so that the menu works and the content from the other components now loads in the div in the body of the page

--- assignment-one/src/app/app-routing.module.ts
+++ assignment-one/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ChannelsComponent } from './channels/channels.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'login', component:LoginComponent }, 
+  { path:'groups', component:GroupsComponent },
+  { path:'channels', component:ChannelsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
