f2b3b0eb5bf9e6caed6e1cbf2fa49a0378943bea
becharvey93 <becharvey93@users.noreply.github.com>
2018-09-06 11:09:09 +1000

    modified logout so that all local storage is reset

--- assignment-one/src/app/login/login.component.ts
+++ assignment-one/src/app/login/login.component.ts
@@ -25,6 +25,12 @@ export class LoginComponent implements OnInit {
     }
   }
 
+// usernames for testing:
+// to test super user privileges = super 123
+// to test group admin privileges = group 123
+// to test general privileges = correctusername password
+// creating a new user will always have general privileges
+
   loginUser(event){
     event.preventDefault();
     if (this.username == "super" && this.password == "123"){
@@ -39,7 +45,7 @@ export class LoginComponent implements OnInit {
       localStorage.setItem("login-type", "group");
       localStorage.setItem("username", this.username)
     }
-    else if (this.username == "correctusername" && this.password =="correctpassword"){
+    else if (this.username == "correctusername" && this.password =="password"){
       this.router.navigateByUrl('/groups');
       localStorage.setItem("login-type", "general");
       localStorage.setItem("username", this.username)
--- assignment-one/src/app/logout/logout.component.ts
+++ assignment-one/src/app/logout/logout.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent implements OnInit {
-
+  router: any;
   constructor() { }
 
   ngOnInit() {
     localStorage.setItem("username", "");
+    localStorage.setItem("login-type", "");
+    localStorage.setItem("password", "");
+    alert("You have now been logged out");
+    this.router.navigateByUrl('/login');
+
   }
 
 }
